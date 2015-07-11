28df813554ac5e60e6986af61f574f5bf3ec7dc1
naframan <naframan@gmail.com>
2015-07-12 00:09:28 +0200

    Cambio mínimo de texto en index.js

--- routes/index.js
+++ routes/index.js
@@ -11,7 +11,7 @@ router.get('/author', function(req, res) {
   res.render('author.ejs', {
     title: 'About myself',
     fotonaf: '/images/naframan.png',
-    chicha: 'Sitio web desarrollado por Antonio Fernández dentro del curso de MiriadaX de desarrollo en la nube con HTML5, Javascript y node.js. Lo sé, el sitio es espartano pero cumple lo exigido por el curso, espero, no tengo tiempo para más. Se agradecerán criticas constructivas sobre el desarrollo, el estilo ya sé que es feo ;)',
+    chicha: 'Sitio web desarrollado por Antonio Fernández dentro del curso de MiriadaX de desarrollo en la nube con HTML5, Javascript y node.js. Lo sé, el sitio es espartano pero cumple lo exigido por el curso, espero, porqué no tengo tiempo para más. Se agradecerán criticas constructivas sobre el desarrollo, el estilo ya sé que es feo ;)',
   });
 });
 
