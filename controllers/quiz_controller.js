eab5479fe4de13c831689a82de830d6aba82a4ba
naframan <naframan@gmail.com>
2015-07-23 17:07:35 +0200

    Ejercicio 8.4 resuelto. Falta corregir tema placeholder vs. value

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -115,6 +115,13 @@ exports.update = function(req, res) {
     }
   );
 };
+
+
+exports.destroy = function (req, res) {
+  req.quiz.destroy().then( function () {
+    res.redirect('/quizes');
+  }).catch(function(error) { next(error)});
+};
 /*
   res.locals.expReg = /^rom[ae]$/i;
   if (res.locals.expReg.test(req.query.respuesta)) {
