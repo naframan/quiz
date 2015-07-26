e931216d14a4d50b01cdc3f3722297fb14d4f3de
naframan <naframan@gmail.com>
2015-07-26 14:32:20 +0200

    Ejercicio 9.4 operativo. Comentarios de usuarios logueados no son automaticamente publicados, tp. sé si se debería hacer así que lo dejo así, solo se publican si se les da a publicar.

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,8 @@ router.get('/', function(req, res) {
 });
 
 // Autoload de comandos con :quizId
-router.param('quizId', quizController.load);  // autoload :quizId
+router.param('quizId',    quizController.load);     // autoload :quizId
+router.param('commentId', commentController.load);  //autoload :commentId
 
 // Definición de rutas de sesión
 router.get('/login',  sessionController.new);       // formulario login
@@ -34,7 +35,12 @@ router.get('/quizes/:quizId(\\d+)/edit',    sessionController.loginRequired, qui
 router.put('/quizes/:quizId(\\d+)',         sessionController.loginRequired, quizController.update);
 router.delete('/quizes/:quizId(\\d+)',      sessionController.loginRequired, quizController.destroy);
 
-router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
-router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/new',
+                                            commentController.new);
+router.post('/quizes/:quizId(\\d+)/comments',
+                                            commentController.create);
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+                                            sessionController.loginRequired,
+                                            commentController.publish);
 
 module.exports = router;
